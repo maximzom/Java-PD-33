697dd1e0939ff3dbb0b2248d65af124c37424c74
maximzom <86885085+maximzom@users.noreply.github.com>
2024-09-05 19:40:21 +0300

    PR1-HOMEWORK

--- untitled/src/main/java/org/example/Product.java
+++ untitled/src/main/java/org/example/Product.java
@@ -12,30 +12,37 @@ public class Product {
         this.name = name;
         this.price = price;
         this.description = description;
-        this.category = category; // Ініціалізація категорії
+        this.category = category;
     }
 
     public int getId() {
         return id;
     }
+    
     public void setId(int id) {
         this.id = id;
     }
+    
     public String getName() {
         return name;
     }
+    
     public void setName(String name) {
         this.name = name;
     }
+    
     public double getPrice() {
         return price;
     }
+    
     public void setPrice(double price) {
         this.price = price;
     }
+    
     public String getDescription() {
         return description;
     }
+    
     public void setDescription(String description) {
         this.description = description;
     }
@@ -43,9 +50,11 @@ public class Product {
     public Category getCategory() {
         return category;
     }
+    
     public void setCategory(Category category) {
         this.category = category;
     }
+    
     @Override
     public String toString() {
         return "Товар{" +
