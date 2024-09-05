481207e55d53a8fe1bec516aafa92c4fe0b42807
maximzom <86885085+maximzom@users.noreply.github.com>
2024-09-05 19:39:06 +0300

    PR1-HOMEWORK

--- untitled/src/main/java/org/example/Category.java
+++ untitled/src/main/java/org/example/Category.java
@@ -1,23 +1,26 @@
 package org.example;
 
 public class Category {
-    private int id; // Унікальний ідентифікатор категорії
-    private String name; // Назва категорії
-    // Конструктор класу
+    private int id;
+    private String name;
+
     public Category(int id, String name) {
         this.id = id;
         this.name = name;
     }
-    // Геттери та сеттери
+
     public int getId() {
         return id;
     }
+    
     public void setId(int id) {
         this.id = id;
     }
+    
     public String getName() {
         return name;
     }
+    
     public void setName(String name) {
         this.name = name;
     }
