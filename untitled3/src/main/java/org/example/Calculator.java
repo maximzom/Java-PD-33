e2e5145c786fb504410bd1302de9e309449e871e
maximzom <86885085+maximzom@users.noreply.github.com>
2024-10-09 23:09:29 +0300

    PR2-CLASSWORK_AND_HOMEWORK

---
+++ untitled3/src/main/java/org/example/Calculator.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Calculator
+{
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Ділення на нуль не допускається.");
+        }
+        return a / b;
+    }
+}
+
